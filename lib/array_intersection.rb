5fce0f5d97b7a7abe83f386ab80472f15972c3b9
RebeccaRoach <rebeccaroach12@gmail.com>
2020-03-22 13:12:40 -0700

    intersection method implemented, tests passing.

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,21 @@
+# find elements that are common to both lists
+# concat list 2 onto list1 to make one large array
+# try to create keys out of every array element
+# if element is already there as a key, it overlaps --> push to result array
+
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  
+  array = list1.concat(list2)
+  hash = {}
+  result = []
+
+  array.each do |item|
+    if hash[item].nil?
+      hash[item] = true
+    else
+      result << item
+    end
+  end
+
+  return result
 end
