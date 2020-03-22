0b2af2795d186952a08dad79e7f0c232c427c863
RebeccaRoach <rebeccaroach12@gmail.com>
2020-03-22 14:24:05 -0700

    Palindrome_permutation method implemented, tests passing.

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,38 @@
+# Write a method which takes a string as an argument and returns true
+# if the letters could be re-arranged into a palindrome.
+# ex:
+# palindrome_permutation?("hello") => false
+# palindrome_permutation?("carrace") => true # because racecar is a palindrome
+
+# return true for empty string
+# create hash with letters as keys, set value to one or increment for each occurrence
+# if there is a value with an odd number of occurences, return false unless there's only one odd value
+# return true
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  return true if string == ""
+
+  array = string.split("")
+  hash = {}
+
+  array.each do |letter|
+    if hash[letter].nil?
+      hash[letter] = 1
+    else
+      hash[letter] += 1
+    end
+  end
+
+  odd_vals = []
+  hash.each do |key, value|
+    if value.odd?
+      odd_vals << [value]
+    end
+  end
+
+  if odd_vals.length > 1
+    return false
+  else
+    return true
+  end
 end
